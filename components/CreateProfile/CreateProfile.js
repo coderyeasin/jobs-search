b5f30a0f6437288c5e2ae364a90a29a13234c550
coderyeasin <yeasin3233@gmail.com>
2022-05-26 19:31:42 +0600

    profile

--- components/CreateProfile/CreateProfile.js
+++ components/CreateProfile/CreateProfile.js
@@ -1,9 +1,29 @@
-import React from "react";
+import Image from "next/image";
+
 
 const CreateProfile = () => {
   return (
-    <div>
-      <h3>Create Profile</h3>
+    <div className="flex justify-center ">
+      <div className="profile_pic ">
+        <Image
+          src="/images/profile.png"
+          alt="profile"
+          height={600}
+          width={638}
+        />
+      </div>
+      <div className="profile_content space-y-10">
+        <h4 className="font-medium text-[25px] leading-8 text-[#F2994A]">
+          Create Profile
+        </h4>
+        <h3 className="font-semibold text-[46px] leading-10 text-[#333333] max-w-[464px]">Build Your Personal Account Profile</h3>
+        <p className="max-w-[689px]">
+          Create an account for the job information you want, get daily
+          notifications and you can easily apply directly to the company you
+          want and create an account now for free
+        </p>
+        <button>Create Account</button>
+      </div>
     </div>
   );
 };
--- components/Subscribe/Subscribe.js
+++ components/Subscribe/Subscribe.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import styles from '../../styles/Home.module.css'
+// import img from "../../public/images/shape.png";
 
 const Subscribe = () => {
     return (
-        <div>
-            <h3>Subscribe</h3>
+      <div className="my-[138px]">
+        <div className="flex justify-evenly items-center bg-[#F2994A] h-[422px] mx-[70px] rounded-[84px]">
+          <h3 className="font-semibold text-5xl leading-relaxed text-white">
+            Never Want to Miss <br /> Any Job News?
+          </h3>
+          <div className="max-w-[576px] p-6 rounded-[52px] bg-white relative z-0">
+            <input
+              type="text"
+              className="border-none "
+              placeholder="Enter your email address here..."
+            />
+            <button className="bg-[#F2994A] w-[130px] h-[45px] text-[21px] font-normal leading-relaxed text-white rounded-[52px] after:absolute after:border-[#E0E0E0] after:border-l-4 after:left-72 after:top-6 after:bottom-6">
+              Subscribe
+            </button>
+          </div>
         </div>
+      </div>
     );
 };
 
